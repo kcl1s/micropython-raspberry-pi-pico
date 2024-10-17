9e6c6cf43fa9f3fa76bfa60305f30c2c261489ac
kcl1s <lohmeyer.keith@gmail.com>
2024-10-17 09:48:29 -0400

    Add files via upload

---
+++ pio_ultrasonic2.py
@@ -0,0 +1,54 @@
+import time
+from machine import Pin
+import rp2
+
+dist = 0
+echo = Pin(21,Pin.IN)
+trig = Pin(20,Pin.OUT)
+pwr = Pin(19, Pin.OUT)
+pwr.on()
+
+def get_cm(x):
+    global dist
+    dist = 300 - sm.get()
+    #print(dist,time.ticks_ms())
+
+@rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
+def ping():
+    label('loop')
+    set(x,0b10010)   # 300 in binary 100101100
+    mov(isr,x)       # 5 msb to isr
+    set(x,0b1100)    # 4 lsb
+    in_(x,4)         # shift into isr
+    mov(x,isr)       # mov isr back to x
+    set(pins,0) [9]  # set trig to low 10 uSec
+    set(pins,1) [9]  # set trig to high 10 uSec
+    set(pins,0)      # set trig to low
+    wait(1,pin,0)    # wait for echo input to go high    
+    label('cm')
+    jmp(x_dec,'ck_echo')    [28]   # ck for timeout < 300
+    jmp('do_delay')        # if so jmp to 80 ms delay
+    label('ck_echo')
+    jmp(pin,'cm')   [28]   # while echo high jmp to dec
+    mov(isr,x)             # when echo goes low send x to isr
+    push()                 # then push to fifo
+    irq(rel(0))            # trigger irq to "get" value
+    label('do_delay')
+    mov(isr,invert(null))    # start timing loop
+    in_(null,12) # 12 bits = 4096 x 20 = 81920 cycles or ~80 ms
+    mov(y,invert(isr))
+    label("delay")
+    jmp(y_dec, "delay")   [19]   # end timing loop
+    jmp('loop')                  # and jmp to top
+    
+    
+    
+sm = rp2.StateMachine(0, ping, freq=1000000, set_base=trig, jmp_pin=echo, in_base=echo)
+sm.irq(get_cm)
+sm.active(1)
+
+while True:
+    time.sleep(.5)
+    print(dist)
+    
+    
